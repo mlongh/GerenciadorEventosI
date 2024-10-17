6c7e4af7df316ddbc5f442c48f8b70a940a08ee6
manulongh <emanuellibarboza@gmail.com>
2024-10-16 21:55:28 -0300

    adjustments

--- src/evento/Evento.java
+++ src/evento/Evento.java
@@ -6,76 +6,58 @@ import java.util.List;
 import participante.Participante;
 
 public class Evento {
-    private String nome;
-    private Date data;
-    private String local;
-    private int capacidadeMaxima;
-    private List<Participante> participantes;
-    private String organizador;
-    private TipoEvento tipoEvento;
+	private String nome;
+	private Date data;
+	private String local;
+	private int capacidadeMaxima;
+	private List<Participante> participantes;
+	private String organizador;
+	public enum TipoEvento {
+		WorkShop, Palestra
+	}
 
-    public enum TipoEvento {
-        WorkShop, Palestra
-    }
+	public Evento(String nome, Date data, String local, int capacidadeMaxima, String organizador) {
+		this.nome = nome;
+		this.data = data;
+		this.local = local;
+		this.capacidadeMaxima = capacidadeMaxima;
+		this.organizador = organizador;
+		this.participantes = new ArrayList<>();
+	}
 
+	public String getNome() {
+		return nome;
+	}
 
-    public Evento(String nome, Date data, String local, int capacidadeMaxima, String organizador) {
-        this.nome = nome;
-        this.data = data;
-        this.local = local;
-        this.capacidadeMaxima = capacidadeMaxima;
-        this.organizador = organizador;
-        this.organizador = organizador;
-        this.participantes = new ArrayList<>();
-    }
+	public Date getData() {
+		return data;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getLocal() {
+		return local;
+	}
 
-    public Date getData() {
-        return data;
-    }
+	public int getCapacidadeMaxima() {
+		return capacidadeMaxima;
+	}
 
-    public String getLocal() {
-        return local;
-    }
+	public String getOrganizador() {
+		return organizador;
+	}
 
-    public int getCapacidadeMaxima() {
-        return capacidadeMaxima;
-    }
+	public void setOrganizador(String organizador) {
+		this.organizador = organizador;
+	}
 
-    public String getOrganizador() {
-        return organizador;
-    }
+	public List<Participante> getParticipantes() {
+		return participantes;
+	}
 
-    public void setOrganizador(String organizador) {
-        this.organizador = organizador;
-    public String getOrganizador() {
-        return organizador;
-    }
-
-    public void setOrganizador(String organizador) {
-        this.organizador = organizador;
-    }
-
-    public List<Participante> getParticipantes() {
-        return participantes;
-    }
-
-    public void adicionarParticipante(Participante participante) {
-        if (participantes.size() < capacidadeMaxima) {
-            participantes.add(participante);
-        } else {
-            System.out.println("Capacidade máxima atingida.");
-        }
-    }
-
-    public void adicionarParticipante(Participante participante) {
-        if (participantes.size() < capacidadeMaxima) {
-            participantes.add(participante);
-        } else {
-            System.out.println("Capacidade máxima atingida.");
-        }
-    }
+	public void adicionarParticipante(Participante participante) {
+		if (participantes.size() < capacidadeMaxima) {
+			participantes.add(participante);
+		} else {
+			System.out.println("Capacidade máxima atingida.");
+		}
+	}
 }
